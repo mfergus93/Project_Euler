20dd149e53357568f1708367ae9f8fa30e8ef2b7
mfergus93 <mcferguson9@gmail.com>
2024-04-26 12:29:28 -0500

    Update Euler_2.py

--- Euler_2.py
+++ Euler_2.py
@@ -1,13 +1,37 @@
 #By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
 
+#Lets start by calculating n fibonacci terms
 def fibonacci(n):
     a=1
     b=2
-    
-    for i in n:
+    c=0
+    for i in range(n):
         c=a+b
         a=b
         b=c
+    
+    return c
 
+#Check if it works, its a hair off
 for i in range(10):
     print(fibonacci(i))
+
+#Lets try modifying the above to do a single fibonacci to 4 million and sum as it goes
+def fibonacci_even_sum():
+    
+    a=1
+    b=2
+    c=0
+    
+    #initialize the sum to 2 so we dont have to consider early edge case
+    even_sum=2
+    while c < 4_000_000:
+        c=a+b
+        a=b
+        b=c
+        
+        if c % 2 == 0:
+            even_sum+=c
+    return even_sum
+
+answer=fibonacci_even_sum()

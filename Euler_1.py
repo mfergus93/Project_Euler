dfdeebd321d35e038bb86bc78e7e014a6e89719b
mfergus93 <mcferguson9@gmail.com>
2024-04-26 11:59:21 -0500

    Create Euler_1.py

---
+++ Euler_1.py
@@ -0,0 +1,16 @@
+#Iterative approach
+sum=0
+for i in range(1000):
+    if i % 3 == 0 or i % 5 == 0:
+        sum+=i
+
+#Direct approach
+def func(n):
+    c=1
+    sum=0
+    while n*c < 1000:
+        sum+=n*c
+        c+=1
+    return sum
+
+sum2=func(3)+func(5)-func(15)
